3072fbe1dcabec59bf8d9e1f7957b5732647eb4f
Colby Rand <80115565+colbyrand@users.noreply.github.com>
2023-06-29 11:42:41 -0700

    Update planet_downloads_organizer.py

--- planet_downloads_organizer.py
+++ planet_downloads_organizer.py
@@ -2,10 +2,10 @@
 #                           Planet Downloads Organizer
 
 # Written by Colby Rand
-# 2023/02/18
+# 2023/06/29
 
-# This script takes the files downloaded through Planet Explorer and organizes them
-# in the following hierarchy: sensor -> date -> image ID -> all files. Before
+# This script takes PlanetScope imagery downloaded through Planet Explorer and organizes
+# them in the following hierarchy: sensor -> date -> image ID -> all files. Before
 # running this script, make sure all downloaded folders are located in a single
 # folder. Run the script by calling python from the terminal and then selecting the
 # folder with the imagery that needs processing:
@@ -18,8 +18,8 @@ import os
 from tkinter import filedialog as fd
 import tkinter as tk
 import shutil
-import tarfile
 from zipfile import ZipFile
+import json
 
 # Get rid of tkinter root window
 root = tk.Tk()
@@ -31,30 +31,30 @@ os.chdir(directoryname)
 
 # Initialize lists used to store the unique dates and catelog IDs encountered
 PlanetScope_dates = []
-PlanetScope_catids = []
+PlanetScope_IDs = []
 planetscope_present = 0     # variables to indicate if PlanetScope images were encountered
 zip_present = 0
 
+# Check if compressed files are present
 with os.scandir(os.getcwd()) as directory:
     for file in directory:
         if file.name.endswith('.zip'):
             zip_present = 1
 
+# Uncompress all the zip files
 if zip_present == 1:
-    # Uncompress all the zip files
     with os.scandir(os.getcwd()) as directory:
         for file in directory:
-            file_name = file.name
-            if file_name.endswith('.zip'):
-                print('Extracting: ' + file_name + '...')
-                with ZipFile(os.getcwd()+'/'+file_name, 'r') as f:
+            if file.name.endswith('.zip'):
+                print('Extracting: ' + file.name + '...')
+                with ZipFile(os.getcwd()+'/'+file.name, 'r') as f:
                     f.extractall('unzip')
 
     os.chdir('unzip')
     with os.scandir(os.getcwd()) as directory:
         for file in directory:
             if file.name.endswith('MACOSX') == False and file.is_dir:
-                shutil.move(os.getcwd()+'/'+file.name, directoryname)
+                shutil.move(file, directoryname)
 
     os.chdir('..')
     with os.scandir(os.getcwd()) as directory:
@@ -62,7 +62,7 @@ if zip_present == 1:
             if file.name.endswith('unzip'):
                 shutil.rmtree(file)
 
-# Move image folders to PlanetScope folders
+# Move image folders to PlanetScope folder
 with os.scandir(os.getcwd()) as directory:
     for file in directory:
         if file.is_dir():
@@ -70,7 +70,7 @@ with os.scandir(os.getcwd()) as directory:
                 if planetscope_present == 0:
                     os.mkdir('PlanetScope')
                 planetscope_present = 1
-                shutil.move(os.getcwd()+'/'+file.name, os.getcwd()+'/PlanetScope')
+                shutil.move(file, 'PlanetScope')
 
 # Organize PlanetScope folders
 with os.scandir(os.getcwd()) as directory:
@@ -81,29 +81,33 @@ with os.scandir(os.getcwd()) as directory:
                 for file2 in directory2:
                     if file2.is_dir:
                         os.chdir(file2.name)    # enter image folder
-                        path1 = os.getcwd()
                         with os.scandir(os.getcwd()) as directory3:
                             for file3 in directory3:
                                 if file3.name == 'PSScene':
                                     os.chdir(file3)     # enter PSScene folder
                                     with os.scandir(os.getcwd()) as directory4:
-                                        for file4 in directory4:        # loop through files in 'PSScene' folder and create image ID folders in the PlanetScope folder
-                                            if file4.name.endswith('Store') == False and file4.name.startswith('PSScene_collection') == False:
-                                                catid = file4.name[0:23]
-                                                if catid not in PlanetScope_catids:
-                                                    PlanetScope_catids.append(catid)
-                                                    print(catid)
-                                                    os.mkdir('../../' + catid)
-                                                os.chdir('../..')
-                                                path=os.getcwd()
-                                                os.chdir(file2.name+'/PSScene')
-                                                shutil.move(os.getcwd()+'/'+file4.name, path+'/'+catid)
+                                        for file4 in directory4:
+                                            if file4.name.endswith('metadata.json'):    # extract info from metadata
+                                                with open(file4) as f:
+                                                    data = f.read()
+                                                parsed_json = json.loads(data)
+                                                image_id = parsed_json['id']
+                                                if image_id not in PlanetScope_IDs:
+                                                    PlanetScope_IDs.append(image_id)
+                                                    os.mkdir('../../' + image_id)
+                                                shutil.move(file4, directoryname+'/PlanetScope/'+image_id)
+                                    with os.scandir(os.getcwd()) as directory4:
+                                        for file4 in directory4:
+                                            shutil.move(file4, directoryname+'/PlanetScope/'+image_id)
                                     os.chdir('..')
                         os.chdir('..')
-            with os.scandir(os.getcwd()) as directory2:     # delete the now empty folders and manifests
+
+            # delete the now empty folders and unnecessary files
+            with os.scandir(os.getcwd()) as directory2:
                 for file2 in directory2:
-                    if file2.is_dir and file2.name not in PlanetScope_catids:
-                        shutil.rmtree(os.getcwd()+'/'+file2.name, ignore_errors=True)
+                    if file2.is_dir and file2.name not in PlanetScope_IDs:
+                        shutil.rmtree(file2, ignore_errors=True)
+
             # Move image ID folders to date folders
             with os.scandir(os.getcwd()) as directory2:
                 for file2 in directory2:
@@ -112,9 +116,9 @@ with os.scandir(os.getcwd()) as directory:
                         if date not in PlanetScope_dates:
                             PlanetScope_dates.append(date)
                             os.mkdir(date)
-                        shutil.move(file2.name, date)
+                        shutil.move(file2, date)
 
 os.chdir(directoryname)
 
-print('PlanetScope IDs: '+str(PlanetScope_catids))
+print('PlanetScope IDs: '+str(PlanetScope_IDs))
 print('PlanetScope dates: '+str(PlanetScope_dates))
